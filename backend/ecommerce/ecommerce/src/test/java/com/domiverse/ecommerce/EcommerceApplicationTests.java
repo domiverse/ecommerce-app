c34c3b45bae5a17a1d3be0459cf73cf47c266e87
Trung Thái <trungthai230700@gmail.com>
2025-06-14 10:40:58 +0700

    Initial commit: folder structure and .gitignore

---
+++ backend/ecommerce/ecommerce/src/test/java/com/domiverse/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domiverse.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
