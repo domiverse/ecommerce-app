9aa15b8a7b31670059ada3d6c4e8bf5e83072dca
Trung Thái <trungthai230700@gmail.com>
2025-06-16 12:58:03 +0700

    feat(frontend): product-list-grid

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   standalone: false,
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit {
