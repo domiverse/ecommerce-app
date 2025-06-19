7bcb1b40629a3c854dd41bc6e1d21f10b3d34b1d
Trung Thái <trungthai230700@gmail.com>
2025-06-19 20:44:54 +0700

    feat(frontend): test logic cho product details

--- frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Route } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Product } from './../../common/product';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-details',
@@ -6,6 +9,26 @@ import { Component } from '@angular/core';
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
+  product!: Product;
+
+  constructor(private productService: ProductService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    })
+  }
+  handleProductDetails() {
+    const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    )
+  }
 
 }
