7697644a3eedb6b80469ffc5ec81c58857eb8c94
Trung Thái <trungthai230700@gmail.com>
2025-06-20 17:29:31 +0700

    feat(frontend) cập nhật metadata cho service

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -63,6 +63,12 @@ export class ProductService {
     interface GetResponseProducts {
       _embedded: {
         products: Product[];
+      },
+      page: {
+        size: number,
+        totalElements: number,
+        totalPages: number,
+        number: number,
       }
     }
 
