b63c4783f190e5175107840a59bf99a81e8e3a9b
Trung Thái <trungthai230700@gmail.com>
2025-06-22 16:57:46 +0700

    feat(frontend) cập nhật toggle cho sidebar

--- frontend/angular-ecommerce/src/app/app.component.ts
+++ frontend/angular-ecommerce/src/app/app.component.ts
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ecommerce';
-  sidebarVisible: boolean = false;
+  // Biến để theo dõi trạng thái của sidebar (mặc định là đang mở)
+  isSidebarCollapsed = false;
+
+  // Phương thức để đảo ngược trạng thái của sidebar
+  toggleSidebar(): void {
+    this.isSidebarCollapsed = !this.isSidebarCollapsed;
+  }
 }
