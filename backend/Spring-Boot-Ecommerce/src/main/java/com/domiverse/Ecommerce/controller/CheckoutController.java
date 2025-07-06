5684bc6292ca36076af3efdba700c08b4a7d73a8
Trung Thái <trungthai230700@gmail.com>
2025-07-06 21:50:33 +0700

    feat(backend) lưu trữ database Checkout

---
+++ backend/Spring-Boot-Ecommerce/src/main/java/com/domiverse/Ecommerce/controller/CheckoutController.java
@@ -0,0 +1,28 @@
+package com.domiverse.Ecommerce.controller;
+
+
+import com.domiverse.Ecommerce.dto.Purchase;
+import com.domiverse.Ecommerce.dto.PurchaseResponse;
+import com.domiverse.Ecommerce.service.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+    private final CheckoutService checkoutService;
+
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return  purchaseResponse;
+    }
+}
--- backend/Spring-Boot-Ecommerce/src/main/java/com/domiverse/Ecommerce/service/CheckoutServiceImpl.java
+++ backend/Spring-Boot-Ecommerce/src/main/java/com/domiverse/Ecommerce/service/CheckoutServiceImpl.java
@@ -1,13 +1,56 @@
 package com.domiverse.Ecommerce.service;
 
+import com.domiverse.Ecommerce.dao.CustomerRepository;
 import com.domiverse.Ecommerce.dto.Purchase;
 import com.domiverse.Ecommerce.dto.PurchaseResponse;
+import com.domiverse.Ecommerce.entity.Customer;
+import com.domiverse.Ecommerce.entity.Order;
+import com.domiverse.Ecommerce.entity.OrderItem;
+import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
+@Service
 public class CheckoutServiceImpl implements CheckoutService {
+
+    private CustomerRepository customerRepository;
+
+public CheckoutServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public PurchaseResponse placeOrder(Purchase purchase) {
-        // Implement your logic here
-        return null;
+
+        //retrieve the order ìnformation from dto
+        Order order = purchase.getOrder();
+
+        //generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        //populate order with orderItems
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
+
+        //populate order with billing and shipping address
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+        //populate customer with order
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
+        //save to the database
+        customerRepository.save(customer);
+
+        //return a response with the tracking number
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+
+        // Generate a random UUID for the order tracking number
+        return java.util.UUID.randomUUID().toString();
+
     }
 }
