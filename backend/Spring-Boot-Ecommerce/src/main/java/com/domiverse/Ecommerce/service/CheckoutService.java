3ed9b7c369b75ac837a7440e7627778768199f5a
Trung Thái <trungthai230700@gmail.com>
2025-07-06 16:09:27 +0700

    feat(backend) cập nhật Class CheckoutServiceiImpl và Interface Checkout

--- backend/Spring-Boot-Ecommerce/src/main/java/com/domiverse/Ecommerce/service/CheckoutService.java
+++ backend/Spring-Boot-Ecommerce/src/main/java/com/domiverse/Ecommerce/service/CheckoutService.java
@@ -1,4 +1,9 @@
 package com.domiverse.Ecommerce.service;
 
+
+import com.domiverse.Ecommerce.dto.Purchase;
+import com.domiverse.Ecommerce.dto.PurchaseResponse;
+
 public interface CheckoutService {
+    PurchaseResponse placeOrder(Purchase purchase);
 }
---
+++ backend/Spring-Boot-Ecommerce/src/main/java/com/domiverse/Ecommerce/service/CheckoutServiceImpl.java
@@ -0,0 +1,13 @@
+package com.domiverse.Ecommerce.service;
+
+import com.domiverse.Ecommerce.dto.Purchase;
+import com.domiverse.Ecommerce.dto.PurchaseResponse;
+
+public class CheckoutServiceImpl implements CheckoutService {
+
+    @Override
+    public PurchaseResponse placeOrder(Purchase purchase) {
+        // Implement your logic here
+        return null;
+    }
+}
