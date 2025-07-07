6e360f8742342c52b4091d74ef5ddf5498915e5e
Trung Thái <trungthai230700@gmail.com>
2025-07-07 14:10:03 +0700

    feat(backend) cập nhật save to database

--- backend/Spring-Boot-Ecommerce/src/main/java/com/domiverse/Ecommerce/dao/ProductCategoryRepository.java
+++ backend/Spring-Boot-Ecommerce/src/main/java/com/domiverse/Ecommerce/dao/ProductCategoryRepository.java
@@ -1,11 +1,11 @@
-package com.domiverse.Ecommerce.dao;
+    package com.domiverse.Ecommerce.dao;
 
-import com.domiverse.Ecommerce.entity.ProductCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
+    import com.domiverse.Ecommerce.entity.ProductCategory;
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+    import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:4200")
-@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
-}
+    @CrossOrigin("http://localhost:4200")
+    @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+    public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+    }
